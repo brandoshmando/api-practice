9f74055d70be089fbaa6d1d4de1b06971c99186e
Brando <brancraft@gmail.com>
2014-12-02 17:36:33 -0500

    Fix stupid typo in details list view

--- snippets/views.py
+++ snippets/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
-from snippets.serializers import SnipperSerializer
+from snippets.serializers import SnippetSerializer
 
 class JSONResponse(HttpResponse):
   """
@@ -11,7 +12,7 @@ class JSONResponse(HttpResponse):
   """
   def __init__(self, data, **kwargs):
     content = JSONRenderer().render(data)
-    kwargs['conent_type'] = 'application/json'
+    kwargs['content_type'] = 'application/json'
     super(JSONResponse, self).__init__(content, **kwargs)
 
 @csrf_exempt
