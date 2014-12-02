8a8d90a44465866f3db88c4cc1c13978425758ba
Brando <brancraft@gmail.com>
2014-12-02 17:14:53 -0500

    Added view for updating, viewing, or deleting a particular snippet

--- snippets/views.py
+++ snippets/views.py
@@ -28,3 +28,30 @@ def snippet_list(request):
       serializer.save()
       return JSONResponse(serializer.data, status=201)
     return JSONResponse(serializer.errors, status=400)
+@csrf_exempt
+def snippet_detail(request, pk):
+  """
+  Retrieve upadate or delete a particular snippet
+  """
+  try:
+    snippet = Snippet.objects.get(pk=pk)
+  except Snippet.DoesNotExist:
+    return HttpResponse(status=404)
+
+  if request.method == 'GET':
+    serializer = SnippetSerializer(snippet)
+    return JSONResponse(serializer.data)
+
+  elif resquest.method == 'POST':
+    data = JSONParser().parse(request)
+    serializer = SnippetSerializer(snippet, data=data)
+    if serializer.is_valid():
+      serializer.save()
+      return JSONResponse(serializer.data)
+    return JSONResponse(serialize.errors, status=400)
+
+  elif request.method == 'DELETE':
+    snippet.delete()
+    return HttpResponse(status=204)
+
+
